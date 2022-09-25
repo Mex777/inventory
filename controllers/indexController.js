8bf3474f302991cd921d933a839bdd4b9d8691b7
Mex777 <fologeaalexandru@gmail.com>
2022-09-25 17:19:51 +0300

    added the navbar

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,10 @@
 const itemModel = require("../models/item");
+const categoriesModel = require("../models/category");
 
 const home = async (req, res) => {
   const items = await itemModel.find({}).limit(5);
-  res.render("index", { items });
+  const categories = await categoriesModel.find({});
+  res.render("index", { items, categories });
 };
 
 module.exports = { home };
