f55ae89918b9f241bf26d1fbc74daf3b2ce56c64
Mex777 <fologeaalexandru@gmail.com>
2022-09-25 22:10:47 +0300

    made the 404 page and added home to the navbar

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,22 +7,20 @@ const defaultRoute = (req, res) => {
 };
 
 const idRoute = async (req, res) => {
+  const categories = await categoryModel.find({});
   if (
     !mongoose.Types.ObjectId.isValid(req.params.id) ||
     !req.params.id.match(/^[0-9a-fA-F]{24}$/)
   ) {
-    res.render("404");
+    res.status(404).render("404", { categories });
     return;
   }
-  // const categoryExists = await categoryModel.exists({ _id: req.params.id });
   const category = await categoryModel.find({ _id: req.params.id });
-  // console.log(categoryExists);
   if (Object.keys(category).length !== 0) {
-    const categories = await categoryModel.find({});
     const items = await itemModel.find({ category: req.params.id });
     res.render("category", { category, categories, items });
   } else {
-    res.render("404");
+    res.status(404).render("404", { categories });
   }
 };
 
