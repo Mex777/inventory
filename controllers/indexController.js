f5f1b1a600b29dfe4eddfb630c0013087a587052
Mex777 <fologeaalexandru@gmail.com>
2022-09-25 16:53:13 +0300

    displayed the first 5 items on the index page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,7 @@
-const home = (req, res) => {
-  const items = [];
+const itemModel = require("../models/item");
+
+const home = async (req, res) => {
+  const items = await itemModel.find({}).limit(5);
   res.render("index", { items });
 };
 
